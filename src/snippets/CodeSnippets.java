35c36dc1aeba43df3e4d70f6096ca247cb161ba1
mochs <martin.ochs@fhws.de>
2017-03-21 11:30:40 +0100

    Created Project with Code-Snippets Class

---
+++ src/snippets/CodeSnippets.java
@@ -0,0 +1,20 @@
+package snippets;
+
+public class CodeSnippets {
+	
+	
+	public CodeSnippets(){}
+
+	public static void main(String[] args) {
+		
+		CodeSnippets codeSnippets = new CodeSnippets();
+		
+		codeSnippets.printSomething();
+
+	}
+	
+	public void  printSomething(){
+		System.out.println("Hello BMC-4");
+	}
+
+}
